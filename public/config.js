b420b4330e2d0b64c0d3d3448783855d61178ae5
koskedk <koskedk@live.com>
2025-08-20 13:02:03 +0300

    fix link

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.RUNTIME_CONFIG = {
     DATA_BANK_URL:"https://analytics.kenyahmis.org/",
-    GIS_URL:"https://maps.kenyhmis.org/",
+    GIS_URL:"https://maps.kenyahmis.org/",
     SELF_SERVICE_URL:"https://analytics.kenyahmis.org/",
 };
